e47bf4751505a6b5399fee854a311e16299208ec
phantomdevi1 <gruzdev_ilya16@mail.ru>
2026-02-06 14:02:17 +0300

    new file

---
+++ admin/tests.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Тесты</title>
+  <link rel="stylesheet" href="../style.css">
+  <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+</head>
+<body>
+        <header class="student_header">
+        <img src="../img/auth_img.png" alt="">
+        <h2>Тесты</h2>
+        <img src="../img/auth_img.png" alt="" class="header_logo_right">
+    </header>
+    <div class="admin-dashboard">
+    <div style="margin: 20px 0;">
+        <a href="dashboard.php" class="back_btn">← Назад</a>
+        <a href="test_add.php" class="add_users-btn">+ Добавить тест</a>
+    </div>
+</body>
+</html>
